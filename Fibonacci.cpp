c140dbe24345ed0ecf47b1c4b56bc66663b852fe
Aklemon <18792656529@163.com>
2016-07-20 23:56:56 +0800

    stack

---
+++ Fibonacci.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//#include<stdio.h>
+//using namespace std;
+//int Fibonacci(int n)
+//{
+//	int fib1 = 1;
+//	int fib2 = 1;
+//	int ret;
+//	if (n == 1 || n == 2)
+//	{
+//		return 1;
+//	}
+//	for (int i = 2; i < n;i++)
+//	{
+//	    ret=fib1 + fib2;
+//		fib1 = fib2;
+//		fib2 = ret;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	int ret=Fibonacci(1);
+//	int ret1 = Fibonacci(5);
+//	int ret2 = Fibonacci(6);
+//	int ret3 = Fibonacci(7);
+//	int ret4= Fibonacci(8);
+//
+//	//printf("%d", ret);
+//	cout << ret << endl;
+//	cout << ret1 << endl;
+//	cout << ret2<< endl;
+//	cout << ret3<< endl;
+//	cout << ret4<< endl;
+//}
+//
--- GetStackMin.cpp
+++ GetStackMin.cpp
@@ -1,40 +1,45 @@
-#include<stack>
+//#include<stack>
+//#include<iostream>
+//using namespace std;
+//class solution
+//{
+//	stack<int> StackData;
+//	stack<int>StackMin;
+//	void Push(int data)
+//	{
+//		StackData.push(data);
+//		if (StackMin.empty())
+//		{
+//			StackMin.push(data);
+//		}
+//		else
+//		{
+//			if (data<StackData.top())
+//			{
+//				StackMin.push(data);
+//			}
+//			else
+//			{
+//				StackMin.push(StackMin.top);
+//			}
+//		}
+//	}
+//	void Pop()
+//	{
+//		StackData.pop();
+//		StackMin.pop();
+//	}
+//	int top()
+//	{
+//		return StackData.top();
+//	}
+//	int MinData()
+//	{
+//		return StackData.top();
+//	}
+//};
+
+
 #include<iostream>
 using namespace std;
-class solution
-{
-	stack<int> StackData;
-	stack<int>StackMin;
-	void Push(int data)
-	{
-		StackData.push(data);
-		if (StackMin.empty())
-		{
-			StackMin.push(data);
-		}
-		else
-		{
-			if (data<StackData.top())
-			{
-				StackMin.push(data);
-			}
-			else
-			{
-				StackMin.push(StackMin.top);
-			}
-		}
-	}
-	void Pop()
-	{
-		StackData.pop();
-		StackMin.pop();
-	}
-	int top()
-	{
-		return StackData.top();
-	}
-	int MinData()
-	{
-		return StackData.top();
-	}
-};
+#include<stack>
